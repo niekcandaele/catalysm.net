49a98846417b3c9a9d07ecffb38d6275017c8575
Niek Candaele <niekcandaele@gmail.com>
2021-02-27 09:08:40 +0100

    Update some stuff :)

--- src/commands.tsx
+++ src/commands.tsx
@@ -1,28 +1,6 @@
-import { FaGithub, FaDiscord, FaPatreon } from "react-icons/fa";
-import { FiMail } from "react-icons/fi";
-import {
-  DiBootstrap,
-  DiCss3,
-  DiDebian,
-  DiDocker,
-  DiDotnet,
-  DiGit,
-  DiJava,
-  DiJavascript1,
-  DiJqueryLogo,
-  DiLaravel,
-  DiLinux,
-  DiMysql,
-  DiNginx,
-  DiNodejsSmall,
-  DiNpm,
-  DiPostgresql,
-  DiPython,
-  DiRasberryPi,
-  DiReact,
-  DiRedis
-} from "react-icons/di";
-import React from "react";
+import React from 'react';
+import { FaDiscord, FaGithub } from 'react-icons/fa';
+import { FiFileText, FiMail } from 'react-icons/fi';
 
 export interface ICommand {
   command: string;
@@ -52,32 +30,32 @@ const commands: Array<ICommand> = [
     output: []
   },
   {
-    command: "cat contactInfo.txt",
+    command: "cat links.txt",
     delay: 250,
     output: [
       {
-        link: "mailto:me@catalysm.net",
+        link: "mailto:niek@candaele.dev",
         type: OutputType.link,
-        text: "me@catalysm.net",
+        text: "niek@candaele.dev",
         prepend: <FiMail />
       },
       {
         link: "https://github.com/niekcandaele",
         type: OutputType.link,
-        text: "niekcandaele",
+        text: "Github",
         prepend: <FaGithub />
       },
       {
-        link: "https://catalysm.net/discord",
+        link: "https://aka.candaele.dev/discord/",
         type: OutputType.link,
-        text: "Catalysm#7488",
+        text: "Discord",
         prepend: <FaDiscord />
       },
       {
-        link: "https://www.patreon.com/catalysm",
+        link: "https://blog.candaele.dev/",
         type: OutputType.link,
-        text: "Catalysm",
-        prepend: <FaPatreon />
+        text: "Blog",
+        prepend: <FiFileText />
       }
     ]
   },
@@ -105,130 +83,13 @@ const commands: Array<ICommand> = [
         type: OutputType.link,
         prepend: "├── ",
         text: "Ban notifier",
-        link: "https://bans.catalysm.net"
-      },
-      {
-        type: OutputType.link,
-        prepend: "├── ",
-        text: "CS:GO config",
-        link: "https://github.com/niekcandaele/Catalysm-CSGO-config"
+        link: "https://github.com/Bantr"
       },
       {
         type: OutputType.link,
         prepend: "└── ",
         text: "CS:GO RankMe interface",
         link: "https://github.com/niekcandaele/CSGO-RankMe-stats"
-      }
-    ]
-  },
-  {
-    command: "cat technologies.txt",
-    prompt: "niek@homeserver:~/code$",
-    delay: 250,
-    output: [
-      {
-        type: OutputType.text,
-        text: "Some of the technologies I've used in the past"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiBootstrap />,
-        text: "Bootstrap"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiCss3 />,
-        text: "CSS"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiDebian />,
-        text: "Debian"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiDocker />,
-        text: "Docker"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiDotnet />,
-        text: ".NET"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiGit />,
-        text: "Git"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiJava />,
-        text: "Java"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiJavascript1 />,
-        text: "Javascript"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiJqueryLogo />,
-        text: "jQuery"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiLaravel />,
-        text: "Laravel"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiLinux />,
-        text: "Linux"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiMysql />,
-        text: "MySQL"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiNginx />,
-        text: "Nginx"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiNodejsSmall />,
-        text: "Nodejs"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiNpm />,
-        text: "NPM"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiPostgresql />,
-        text: "PostgreSQL"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiPython />,
-        text: "Python"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiRasberryPi />,
-        text: "Raspberry Pi"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiReact />,
-        text: "React"
-      },
-      {
-        type: OutputType.text,
-        prepend: <DiRedis />,
-        text: "Redis"
       }
     ]
   }
